afb3460dbc9096011eab144721cb463d8f149da3
AdrienCharneau <48357527+AdrienCharneau@users.noreply.github.com>
2019-03-10 22:10:49 +0100

    Update LotteryDraw.cs

--- LotteryDraw.cs
+++ LotteryDraw.cs
@@ -5,8 +5,6 @@ namespace LotteryDraw
 {
     class Program
     {
-
-
         /* 
             - What is this?
             
@@ -17,9 +15,9 @@ namespace LotteryDraw
             - What it does?
         
             When executed, this program will a open a console window 
-            where 7 numbers ranging from 1 to 49 will be displayed. It 
-            is important to note that all numbers are unique and not a
-            single one of them is equal to any of the others.
+            where 7 numbers ranging from 1 to 49 (included) will be 
+            displayed. It is important to note that all numbers are 
+            unique and not equal to any of the others.
         */
 
 
@@ -28,9 +26,10 @@ namespace LotteryDraw
         static int[] lotteryTable = new int[7];
 
 
-        // Main method. First we call Draw() in order to work out the 
-        // lottery draw, then we use the Display() method to show the 
-        // winning numbers on the console.
+        // Main method:
+        // First we call Draw() in order to work out the lottery draw, 
+        // then we use the Display() method to show the winning 
+        // numbers on the console.
         static void Main(string[] args)
         {
             Draw();
@@ -38,30 +37,54 @@ namespace LotteryDraw
         }
 
 
+        // Draw method:
+        // This method fills the Array 'lotteryTable' with 7 unique 
+        // random numbers ranging from 1 to 49 (included).
         static void Draw()
         {
+
+            // Initialization of an int variable that counts how many 
+            // winning numbers are contained inside 'lotteryTable'.
             int lotteryCounter = 0;
+
+            // Declaration of a random number variable that will be
+            // used to get winning numbers.
             Random rndNumber = new Random();
+
+            // Declaration of an int variable that will store values
+            // found by 'rndNumber'.
             int winNumber;
 
             while (lotteryCounter < lotteryTable.Length)
             {
+                // Setting 'winNumber' to a random value ranging from 
+                // 1 to 49 (included).
                 winNumber = rndNumber.Next(1, 50);
 
                 for (int i = 0; i < lotteryTable.Length; i++)
                 {
                     if (lotteryTable[i] == winNumber)
                     {
+                        // At any moment, if a value is already present
+                        // in 'lotteryTable', the "for" loop breaks and 
+                        // we set 'winNumber' to a new random value.
                         break;
                     }
                     else if (i == lotteryCounter)
                     {
+                        // When we have finished checking all values 
+                        // in 'lotteryTable', we add 'winNumber' to the
+                        // Array and restart the process with a new 
+                        // random value.
                         lotteryTable[lotteryCounter] = winNumber;
                         lotteryCounter++;
                         break;
                     }
                     else
                     {
+                        // At any moment, if a value is not present in
+                        // 'lotteryTable', the "for" loop continues and
+                        // we check the next value present in the Array.
                         continue;
                     }
                 }
@@ -69,12 +92,15 @@ namespace LotteryDraw
         }
 
 
+        // Display method:
+        // This method displays every single number present in 
+        // 'lotteryTable' along with their order inside the Array.
         static void Display()
         {
             for (int x = 0; x < lotteryTable.Length; x++)
             {
                 int displayNumber = x + 1;
-                Console.WriteLine("Lottery Index " + displayNumber + " = " + lotteryTable[x]);
+                Console.WriteLine("Lottery number " + displayNumber + " = " + lotteryTable[x]);
             }
         }
 
