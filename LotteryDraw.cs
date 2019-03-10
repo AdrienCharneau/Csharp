e1ad2d42e1394e48b4a6c85d90ce250d98da62b9
AdrienCharneau <48357527+AdrienCharneau@users.noreply.github.com>
2019-03-10 20:24:12 +0100

    Update LotteryDraw.cs

--- LotteryDraw.cs
+++ LotteryDraw.cs
@@ -5,14 +5,35 @@ namespace LotteryDraw
 {
     class Program
     {
+
+
+        /* - What is this?
+            
+           This program is part of an exercice from an online C# course
+           (available at openclassrooms.com) named "Réalisez un tirage 
+           de loto" aka "Build your own Lottery Draw algorithm".
+
+        */
+
+
+        /* - What it does?
         
-        
-        static Random rndNumber = new Random();
-        static int winNumber;
+            When executed, this program will a open a console window 
+            where 7 numbers ranging from 1 to 49 will be displayed. It 
+            is important to note that all numbers are unique and not a
+            single one of them is equal to any of the others.
+
+        */
+
+
+        // Declaring a single-dimensional Array that will contain all 
+        // the chosen numbers.
         static int[] lotteryTable = new int[7];
-        static int lotteryCounter = 0;
 
 
+        // Main method. First we call Draw() in order to work out the 
+        // lottery draw, then we use the Display() method to show the 
+        // winning numbers on the console.
         static void Main(string[] args)
         {
             Draw();
@@ -22,17 +43,21 @@ namespace LotteryDraw
 
         static void Draw()
         {
+            int lotteryCounter = 0;
+            Random rndNumber = new Random();
+            int winNumber;
+
             while (lotteryCounter < lotteryTable.Length)
             {
                 winNumber = rndNumber.Next(1, 50);
 
-                for (int numero = 0; numero < lotteryTable.Length; numero++)
+                for (int i = 0; i < lotteryTable.Length; i++)
                 {
-                    if (lotteryTable[numero] == winNumber)
+                    if (lotteryTable[i] == winNumber)
                     {
                         break;
                     }
-                    else if (numero == lotteryCounter)
+                    else if (i == lotteryCounter)
                     {
                         lotteryTable[lotteryCounter] = winNumber;
                         lotteryCounter++;
@@ -51,11 +76,11 @@ namespace LotteryDraw
         {
             for (int x = 0; x < lotteryTable.Length; x++)
             {
-                int affichage = x + 1;
-                Console.WriteLine("Index Loto " + affichage + " = " + lotteryTable[x]);
+                int displayNumber = x + 1;
+                Console.WriteLine("Lottery Index " + displayNumber + " = " + lotteryTable[x]);
             }
         }
-        
-        
+
+
     }
 }
